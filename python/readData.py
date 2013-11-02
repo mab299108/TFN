93487618377f4938976ec99b9718b6ea2bb97c79
mab299108 <mab299108@yahoo.com>
2013-11-02 02:32:35 -0500

    test ç

--- python/readData.py
+++ python/readData.py
@@ -3,11 +3,41 @@ import datetime
 
 ser=serial.Serial('/dev/tty.usbmodem1411',9600)
 count = 0
-bulkTemp = 0
-bulkFlow = 0
 myData={}
+nameList=[]
+typeList=[]
 
-print("Starting:"+str(datetime.datetime.now().time()))
+def addData( vName, vValue ):
+	global myData
+	try:
+		myData[vName]=myData[vName]+float(vValue)
+		myData[vName+"count"]=myData[vName+"count"]+1
+	except KeyError:
+		# Key is not present
+		myData[vName]=float(vValue)
+		myData[vName+"count"]=1
+	pass
+def formatOutput(i):
+	global myData
+	global nameList
+	myKey = nameList[i]
+	vOut = myData[myKey]/myData[myKey+"count"]
+	myData[myKey] = 0
+	myData[myKey+"count"] = 0
+	return str(vOut)
+
+def getTime():
+	return str(datetime.datetime.now().time())
+
+def buildLists(vName, vItem):
+	global nameList
+	global typeList
+	if vName not in nameList:
+		nameList.append(vName)
+		typeList.append(vItem)
+
+
+print("Starting:"+getTime())
 while 1:
 
 	#print(count)
@@ -16,26 +46,22 @@ while 1:
 	vItem,vName,vValue = SerIn.split(":")
 	
 	if vItem == "T":
-		#print("temp")
-		#print(vValue)
-		bulkTemp=bulkTemp+float(vValue)
-		myData[vName]=bulkTemp
+		addData( vName, vValue )
+		buildLists(vName, vItem)
 
 	elif vItem == "F":
-		bulkFlow = bulkFlow+float(vValue)
-		#assumes count iterated by temp
-		myData[vName]=bulkFlow    
-
+		addData( vName, vValue )
+		buildLists(vName, vItem)
+		
 	else:
-		print("other"+str(SerIn)+ " at "+str(datetime.datetime.now().time()))	
+		print("other"+str(SerIn)+ " at "+getTime())	
 
 	count=count+1
 
 	if count == 5:
-		divisor = count/2
-		print("averages  at "+str(datetime.datetime.now().time()))
-		print("TEMP: - "+str(bulkTemp/divisor))
-		print("FLOW: - "+str(bulkFlow/divisor))
+		print("averages  at "+getTime())
+		for i in xrange(len(nameList)):
+			print(typeList[i]+": "+nameList[i]+" - "+formatOutput(i))
+
+		#print(nameList)
 		count = 0
-		bulkTemp = 0
-		bulkFlow = 0
